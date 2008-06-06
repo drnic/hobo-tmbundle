be75eecf9068b2f2146beecd324bdaed02aa8a90
Dr Nic Williams <drnicwilliams@gmail.com>
2008-06-06 21:11:40 +1000

    rails_root is nil if file outside a rails app

--- Support/test/test_rails_helpers.rb
+++ Support/test/test_rails_helpers.rb
@@ -3,15 +3,22 @@ require File.dirname(__FILE__) + '/../lib/rails_helper'
 
 class TestRailsHelper < Test::Unit::TestCase
   context "Hobo app using Hobo gems" do
-    setup do
-      @expected_rails_root = File.expand_path(File.dirname(__FILE__) + "/fixtures/fixture_apps/app_using_hobo_gems")
-      FileUtils.chdir @expected_rails_root
-    end
-
     should "find RAILS_ROOT" do
-      actual = RailsHelper.rails_root(File.join(@expected_rails_root, "app/controllers/application.rb"))
-      assert_equal(@expected_rails_root, actual)
+      expected = File.expand_path(File.dirname(__FILE__) + "/fixtures/fixture_apps/app_using_hobo_gems")
+      actual = RailsHelper.rails_root(File.join(expected, "app/controllers/application.rb"))
+      assert_equal(expected, actual)
     end
   end
+  
+  context "Random file outside of a Rails app" do
+    setup do
+      FileUtils.chdir "/tmp"
+    end
+
+    should "not find any RAILS_ROOT" do
+      assert_nil(RailsHelper.rails_root("/tmp"))
+    end
+  end
+  
   
 end
